dd9f213dc0225c5f481f49179f7800830a74bcf9
viniferraria <vferraria10@gmail.com>
2020-09-28 20:58:50 -0300

    removing x-powered-by headers

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ app.options('*', cors());
 app.use(express.json());
 app.use(routes);
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`)
