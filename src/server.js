8086a155a82e41163a8363f2a71417d4e248cb5f
viniferraria <vferraria10@gmail.com>
2020-09-28 13:13:25 -0300

    removing x-powered-by from headers

--- src/server.js
+++ src/server.js
@@ -1,18 +1,24 @@
+'use strict'
+
 const express = require('express');
-const routes = require('./routes')
-const cors = require('cors')
+const routes = require('./routes');
+const cors = require('cors');
+const helmet = require('helmet');
+
 
 require('dotenv').config();
 require('./database');
 
 const app = express();
 
+app.use(helmet());
+app.disable('x-powered-by');
 app.use(cors());
 app.options('*', cors());
 app.use(express.json());
 app.use(routes);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`)
