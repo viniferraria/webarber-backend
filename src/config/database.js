5555353a59ecffe81c5faeaf9a7deff7ec1cd093
viniferraria <vferraria10@gmail.com>
2020-10-02 18:49:57 -0300

    Changes on workflow

--- src/config/database.js
+++ src/config/database.js
@@ -4,9 +4,9 @@ module.exports = {
     dialect: process.env.DB_DIALECT || 'postgres',
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 5432,
-    username: process.env.DB_USERNAME || 'webarber',
-    password: process.env.DB_PASSWORD || 'webarber',
-    database: process.env.DB_NAME || 'webarber',
+    username: process.env.DB_USERNAME || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres',
+    database: process.env.DB_NAME || 'postgres',
     define: {
         timestamps: true, 
         underscored: false,
