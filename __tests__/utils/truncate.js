5ac149f594f678e9ed4245c82d42225186df72bc
viniferraria <vferraria10@gmail.com>
2020-10-05 18:49:57 -0300

    Created tests for UserController

--- __tests__/utils/truncate.js
+++ __tests__/utils/truncate.js
@@ -1,8 +1,8 @@
-const { sequelize } = require('../../src/app/models');
+// const { sequelize } = require('../../src/app/models');
 
-module.exports =  () => {
-    return Promise.all(Object.keys(sequelize.models).map(key => {
-        return sequelize.models[key].destroy({ truncate: true, force: true });
-        })
-    );
-}
+// module.exports =  () => {
+//     return Promise.all(Object.keys(sequelize.models).map(key => {
+//         return sequelize.models[key].destroy({ truncate: true, force: true });
+//         })
+//     );
+// }
--- jest.config.js
+++ jest.config.js
@@ -18,7 +18,7 @@ module.exports = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ["src/**", "!src/database/migrations/**"],
+  collectCoverageFrom: ["src/**", "!src/database/migrations/**", "!src/database/seeders/**"],
   // The directory where Jest should output its coverage files
   coverageDirectory: "__tests__/coverage",
 
