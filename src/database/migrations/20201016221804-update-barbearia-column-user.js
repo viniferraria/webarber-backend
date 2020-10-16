b6c1d81b330ffbd9716f0ac0dcf6107104dee014
Zewu Chen <39866572+zewuchen@users.noreply.github.com>
2020-10-16 20:43:50 -0300

    Fixed atribbutes and tests

---
+++ src/database/migrations/20201016221804-update-barbearia-column-user.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn(
+      'Barbearia', 
+      'user_id',
+      {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Usuario',
+          key: 'id'
+        }
+      }
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Barbearia', 'user_id');
+  }
+};
