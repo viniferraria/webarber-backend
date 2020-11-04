3602cecff9fd142747bb179a69dd031c305a8f45
Zewu Chen <39866572+zewuchen@users.noreply.github.com>
2020-11-03 22:20:29 -0300

    Criado a tabela de status de agendamento

---
+++ src/database/migrations/20201104011733-create-status-agendamento.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('StatusAgendamento', {
+            id: {
+                type: Sequelize.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false,
+            },
+            nome: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+        }, {
+            timestamps: false,
+            freezeTableName: true
+        });
+        await queryInterface.bulkInsert('StatusAgendamento', [
+            {
+                id: 1,
+                nome: "Agendando"
+            },
+            {
+                id: 2,
+                nome: "Andamento"
+            },
+            {
+                id: 3,
+                nome: "Concluído"
+            },
+            {
+                id: 4,
+                nome: "Cancelado"
+            }]);
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('StatusAgendamento');
+    }
+};
