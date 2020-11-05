291140d65b16b130b8048cbed8a7bc7785dc5236
Zewu Chen <39866572+zewuchen@users.noreply.github.com>
2020-11-04 22:28:18 -0300

    Feature/status agendamento (#64)
    * Criado a tabela de status de agendamento
    
    * Adicionado testes, rotas e fixado o banco de dados
    
    Co-authored-by: Vinícius Amendoeira Ferraria <39192461+viniferraria@users.noreply.github.com>

--- __tests__/cases.js
+++ __tests__/cases.js
@@ -41,9 +41,14 @@ let ServicoTeste = {
     "barbearia_id": 1
 };
 
+var StatusAgendamentoTeste = {
+    "nome": "Agendado"
+};
+
 module.exports = {
     UsuarioTeste,
     ModeradorTeste,
     BarbeariaTeste,
     ServicoTeste,
+    StatusAgendamentoTeste
 };
