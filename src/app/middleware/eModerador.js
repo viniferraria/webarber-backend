909ed9928c2e7c706d64745d56294b8d1a0f6053
viniferraria <vferraria10@gmail.com>
2020-11-22 20:29:58 -0300

    Correçao dos middlewares

--- src/app/middleware/eModerador.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res, next) => {
-    const { idTipo } = req;
-    if (idTipo !== 2) {
-        return res.status(401).json({ message: "Rota restrita à moderadores" });
-    }
-        
-    return next();
-};
