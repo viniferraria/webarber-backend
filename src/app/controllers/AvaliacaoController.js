8b8689558c98b98f343034635bc15d8362eeadb5
viniferraria <vferraria10@gmail.com>
2020-11-22 18:34:30 -0300

    Corrigindo erros codacy

--- src/app/controllers/AvaliacaoController.js
+++ src/app/controllers/AvaliacaoController.js
@@ -57,7 +57,6 @@ module.exports = {
 
             return res.status(200).json(avaliacao);
         } catch (error) {
-            console.log(error);
             return res.status(400).json({ message: "Erro ao cadastrar avaliação" });
         }
     },
