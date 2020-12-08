d5fdcddda517c51a91ecc2200842d7675c0b09dc
viniferraria <vferraria10@gmail.com>
2020-12-08 20:05:25 -0300

    Usando secrets do action

--- __tests__/unit/Usuario.test.js
+++ __tests__/unit/Usuario.test.js
@@ -1,39 +1,17 @@
 const { Usuario } = require("../../src/app/models");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const { promisify } = require("util");
 
-let mockUsuario = {
-    id: 1,
-    nome: "viniteste1",
-    sobrenome: "teste",
-    email: "viniteste@teste.com",
-    password: "123456",
-    idTipo: 1
-};
+describe( "User", () => {} );
 
-let usuario;
-
-describe("User", () => {
-    describe( "Teste senha", () => {
-        it("Deve criptografar a senha de um usuário", async () => {
-            usuario = await Usuario.create(mockUsuario);
-            mockUsuario.id = usuario.id;
-            const compareHash = await bcrypt.compare(mockUsuario.password, usuario.password_hash);
-            expect(compareHash).toBe(true);
-        });
+it("should encrypt user password", async () => {
+    const user = await Usuario.create({
+        nome: "viniteste1",
+        sobrenome: "teste",
+        email: "viniteste@teste.com",
+        password: "123456",
+        idTipo: 1
     });
-
-    describe("Teste token", () => {
-        it("Deve gerar um token válido para um usuário", async () => {    
-            try {
-                usuario.generateToken();
-                let decoded = await promisify(jwt.verify)(usuario.sessionToken, process.env.APP_SECRET);
-                expect(decoded.id).toBe(mockUsuario.id);
-                expect(decoded.idTipo).toBe(mockUsuario.idTipo);
-            } catch (err) {
-                console.error(err);
-            }
-        });
-    });
+    
+    const compareHash = await bcrypt.compare("123456", user.password_hash);
+    expect(compareHash).toBe(true);
 });
